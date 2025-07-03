51f12c63a65e0dbedfadc0038b9b5722275ed697
Mesan237 <negokamgaing@gmail.com>
2025-07-03 09:34:57 +0100

    reset project

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default Index
+
+const styles = StyleSheet.create({})
