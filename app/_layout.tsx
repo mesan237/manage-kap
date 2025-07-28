c441841b4172670f8cd1f7b42c1c707d58f1cf62
mesan237 <negokamgaing@gmail.com>
2025-07-28 13:07:25 +0100

    creating the build with eas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,21 +3,15 @@ import { StatusBar } from 'react-native';
 import { Slot } from 'expo-router';
 import './globals.css';
 import { PowerSyncProvider } from '@/powersync/PowersyncProvider';
-import { useEffect } from 'react';
-// import { useSystem } from '@/powersync/system';
 import { SystemProvider } from '@/powersync/SystemProvider';
 
 export default function RootLayoutNav() {
-  // const system = useSystem();
-
-  // useEffect(() => {
-  //   system.init(); // Initialize system when component mounts
-  // }, []);
-
   return (
-    <>
-      <StatusBar hidden={true} />
-      <Slot />
-    </>
+    <SystemProvider>
+      <PowerSyncProvider>
+        <StatusBar hidden={true} />
+        <Slot />
+      </PowerSyncProvider>
+    </SystemProvider>
   );
 }
